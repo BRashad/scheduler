61b5ddbeffa884436207466d4a52e245364bc9ce
Rashad Bayram <rashadabayram.com>
2021-05-10 18:10:17 -0600

    buttons components added

--- stories/index.js
+++ stories/index.js
@@ -6,19 +6,29 @@ import { action } from "@storybook/addon-actions";
 import "index.scss";
 
 import Button from "components/Button";
+import DayListItem from "components/DayListItem";
 
 storiesOf("Button", module)
-  .addParameters({
-    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
-  })
-  .add("Base", () => <Button>Base</Button>)
-  .add("Confirm", () => <Button confirm>Confirm</Button>)
-  .add("Danger", () => <Button danger>Cancel</Button>)
-  .add("Clickable", () => (
-    <Button onClick={action("button-clicked")}>Clickable</Button>
-  ))
-  .add("Disabled", () => (
-    <Button disabled onClick={action("button-clicked")}>
-      Disabled
-    </Button>
-  ));
+	.addParameters({
+		backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+	})
+	.add("Base", () => <Button>Base</Button>)
+	.add("Confirm", () => <Button confirm>Confirm</Button>)
+	.add("Danger", () => <Button danger>Cancel</Button>)
+	.add("Clickable", () => (
+		<Button onClick={action("button-clicked")}>Clickable</Button>
+	))
+	.add("Disabled", () => (
+		<Button disabled onClick={action("button-clicked")}>
+			Disabled
+		</Button>
+	));
+
+export default function DayListItem(props) {
+	return (
+		<li>
+			<h2 className='text--regular'>Day Name</h2>
+			<h3 className='text--light'>X spots remaining</h3>
+		</li>
+	);
+}
