3115c6898182eb9248ab745297652014b85b90d8
Rashad Bayram <rashadabayram.com>
2021-05-19 16:50:59 -0600

    debugging and decisions tests added

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -10,6 +10,7 @@ import {
 	getAllByTestId,
 	getByAltText,
 	getByPlaceholderText,
+	queryByText,
 } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -28,24 +29,48 @@ describe("Form", () => {
 	});
 
 	it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-		const { container, debug } = render(<Application />);
+		const { container, queryAllByAltText, debug } = render(<Application />);
+
 		await waitForElement(() => getByText(container, "Archie Cohen"));
-
 		const appointments = getAllByTestId(container, "appointment");
-		//console.log(prettyDOM(appointments));
+		const appointment = appointments[1];
 
-		const appointment = getAllByTestId(container, "appointment")[0];
-		//console.log(prettyDOM(appointment));
+		fireEvent.click(queryAllByAltText("Add")[0]);
 
-		fireEvent.click(getByAltText(appointment, "Add"));
-
-		fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+		fireEvent.change(getByPlaceholderText(container, /enter student name/i), {
 			target: { value: "Lydia Miller-Jones" },
 		});
-		fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+		fireEvent.click(getByAltText(container, "Sven Jones"));
 
-		fireEvent.click(getByText(appointment, "Save"));
+		fireEvent.click(getByText(container, "Save"));
+
+		console.log(prettyDOM(appointment));
 		debug();
-		expect(getByText(appointment, "SAVING")).toBeInTheDocument();
+		expect(getByText(container, "SAVING")).toBeInTheDocument();
+		await waitForElement(() => getByText(container, "Lydia Miller-Jones"));
+		//expect(getByText(appointment, "SAVING")).not.toBeInTheDocument();
 	});
+	// it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+	// 	const { container, debug } = render(<Application />);
+	// 	await waitForElement(() => getByText(container, "Archie Cohen"));
+	// 	const appointment = getAllByTestId(container, "appointment")[0];
+	// 	// await waitForElement(() => getByText(container, "Lydia Miller-Jones"));
+	// 	// await waitForElement(() => queryByText(container, "Lydia Miller-Jones"));
+	// 	const appointments = getAllByTestId(container, "appointment");
+	// 	//console.log(prettyDOM(appointments));
+	// 	const addButton = getByAltText(container, "Add");
+	// 	//console.log(prettyDOM(appointment));
+	// 	console.log(prettyDOM(addButton));
+	// 	fireEvent.click(addButton);
+
+	// 	fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+	// 		target: { value: "Lydia Miller-Jones" },
+	// 	});
+	// 	fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+	// 	fireEvent.click(getByText(appointment, "Save"));
+	// 	//	debug();
+	// 	// expect(getByText(appointment, "SAVING")).toBeInTheDocument();
+	// 	expect(getByText(appointment, "SAVING")).not.toBeInTheDocument();
+	// });
 });
